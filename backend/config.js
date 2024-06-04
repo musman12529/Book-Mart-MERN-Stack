6934e5d656ab558685abf9d368ca9c580786d692
Muhammad Usman <m.usman12529@gmail.com>
2024-06-04 01:05:12 -0230

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:usman123@book-store.x2eqcx7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store'
